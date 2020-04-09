67616a227a66e358ddeb213c29dce5f0e1257e52
Scot Sexton <rss3509@gmail.com>
2020-04-09 20:38:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,9 @@
 require 'pry'
 
 def start_game(player1, player2)
-  p "Hello #{player1} & #{playar2}"
+  puts "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player2)
-  start_game = play_game
-   "#{player1} is better than #{player2}"
+  puts "#{player1} is better than #{player2}"
 end
