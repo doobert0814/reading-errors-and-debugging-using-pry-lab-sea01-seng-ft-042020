b0732f3e82ea4e64e919bef4f17769a46503ae66
Scot Sexton <rss3509@gmail.com>
2020-04-09 20:28:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,10 +11,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num =1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+  if "1.) Eat a hamburger."
+     puts "YUM YUM MUNCH MUNCH MUNCH"
+  elsif "2.) Eat a ham." 
+     puts "HAM HAM HAM IN MY TUMMY"
   end
   binding.pry
 end
